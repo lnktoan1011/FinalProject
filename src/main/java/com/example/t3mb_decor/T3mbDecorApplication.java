a6a4ab05740fac96f7c6db895e7fe0381c176e78
Nguyen Cong Thanh <ncthanh0411@gmai.com>
2020-09-12 14:23:02 +0700

    thanh1

---
+++ src/main/java/com/example/t3mb_decor/T3mbDecorApplication.java
@@ -0,0 +1,13 @@
+package com.example.t3mb_decor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3mbDecorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T3mbDecorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/t3mb_decor/T3mbDecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t3mb_decor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3mbDecorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

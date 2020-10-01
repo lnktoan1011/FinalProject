5c157c5e6f205465da058b71272af479f1cd2aea
duonggiabao <duonggiabao0708@gmail.com>
2020-10-01 21:21:01 +0700

    bao 1/10 login 9:20

--- src/main/java/com/example/t3mb_decor/controller/LoginController.java
+++ src/main/java/com/example/t3mb_decor/controller/LoginController.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
     @GetMapping
-    public String login(){return "/layouts/login/login";}
+    public String login(){return "/content/login/login";}
 }

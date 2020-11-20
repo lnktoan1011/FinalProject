b4cfa5220811476f10730ee65eb0598213eed779
duonggiabao <duonggiabao0708@gmail.com>
2020-11-20 22:13:03 +0700

    bao 20/11 done 404 500 error 1012

--- src/main/java/com/example/t3mb_decor/security/SecurityConfiguration.java
+++ src/main/java/com/example/t3mb_decor/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/home","/collections/**","/contact","/about","/product/**").permitAll()
                 .antMatchers(
                 "/admin/**","/static/**","/resources/**","/main/**",
-                "/login/**","/about/**","/images/**").permitAll()
+                "/login/**","/about/**","/images/**","/errors/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()

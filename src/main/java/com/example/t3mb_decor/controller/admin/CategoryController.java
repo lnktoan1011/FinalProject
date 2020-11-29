6e163a7bddce290b0d211f590d46c85dff19313c
duonggiabao <duonggiabao0708@gmail.com>
2020-11-29 20:37:33 +0700

    bao 29/11 cart 837

--- src/main/java/com/example/t3mb_decor/controller/admin/CategoryController.java
+++ src/main/java/com/example/t3mb_decor/controller/admin/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import java.util.Date;
@@ -64,16 +65,18 @@ public class CategoryController {
         return "redirect:/admins/category";
     }
     @PostMapping("add")
-    public String saveCategory(@ModelAttribute("cate") Category category){
+    public String saveCategory(@ModelAttribute("cate") Category category, RedirectAttributes redirectAttributes){
         String id;
 
         //Save cate create date.Because when update the cate create date not save.
         if(category.getId() != 0) {
             categoryService.updateCategory(category);
+            redirectAttributes.addFlashAttribute("success","Category is updated successfully");
             return "redirect:/admins/category/update/" + category.getId() ;
         }
         categoryService.saveCategory(category);
-        return "redirect:" ;
+        redirectAttributes.addFlashAttribute("success","Category is saved successfully");
+        return "redirect:/admins/category" ;
     }
     @GetMapping("update/{id}")
     public String viewUpdateCategory(@PathVariable("id") long id, Model model){
@@ -93,10 +96,11 @@ public class CategoryController {
         return "content/admin/categories";
     }
     @PostMapping("add-sub/{id}")
-    public String addSubCategory(@PathVariable("id") long id,@ModelAttribute("subCate") SubCategory sub){
+    public String addSubCategory(@PathVariable("id") long id,@ModelAttribute("subCate") SubCategory sub, RedirectAttributes redirectAttributes){
         Category category = categoryService.getCategory(id) ;
         sub.setCategory(category);
         subCategoryService.saveSubCategory(sub);
+        redirectAttributes.addFlashAttribute("success","Sub-Category is saved successfully");
         return "redirect:/admins/category/add-sub/" + id ;
     }
 
@@ -110,8 +114,9 @@ public class CategoryController {
         return "content/admin/categories";
     }
     @PostMapping("update-sub/{id}")
-    public String UpdateSubCategory(@PathVariable("id") long id, @ModelAttribute("subCateUpdate") SubCategory sub){
+    public String UpdateSubCategory(@PathVariable("id") long id, @ModelAttribute("subCateUpdate") SubCategory sub, RedirectAttributes redirectAttributes){
         subCategoryService.updateSubCategory(sub);
+        redirectAttributes.addFlashAttribute("success","Sub-Category is updated successfully");
         return "redirect:/admins/category/update-sub/" + id ;
     }
 }
